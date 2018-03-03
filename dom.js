c056b6e3ccfdc814ff177fc8c85d0c6fa229bd99
Alex <alexandutu86@gmail.com>
2018-03-03 17:57:48 +0200

    score

--- dom.js
+++ dom.js
@@ -10,78 +10,123 @@ var congratsPage = document.getElementById("idAndRepo");
 var repoCount = document.getElementById("repoNr");
 var letsGo = document.getElementById("nextButton");
 var userError = document.getElementById("formError");
-var myQuestion = document.getElementById('question');
+var myQuestion = document.getElementById("question");
 var inputForm = document.getElementById("inputEditor");
-var NextQuestion = document.getElementById('BtnStart');
-var lastPage = document.getElementById('resultPage');
+var NextQuestion = document.getElementById("BtnStart");
+var lastPage = document.getElementById("resultPage");
 
-
-var questions = [
+var list = [
   {
-    text: 'What is the correct JavaScript syntax to change the content of the HTML element below? <br> < p id="demo" >This is a demonstration.< /p >',
-    answers: [{ text: 'document.getElementByName("p").innerHTML = "Hello World!"', value: 0 }, { text: '#demo.innerHTML = "Hello World!"', value: 0 }, { text: 'document.getElementById("demo").innerHTML = "Hello World!"', value: 1 }]
+    questions:
+      'What is the correct JavaScript syntax to change the content of the HTML element below? <br> < p id="demo" >This is a demonstration.< /p >',
+    answers: [
+      {
+        text: 'document.getElementByName("p").innerHTML = "Hello World!"',
+        value: 0
+      },
+      { text: '#demo.innerHTML = "Hello World!"', value: 0 },
+      {
+        text: 'document.getElementById("demo").innerHTML = "Hello World!"',
+        value: 1
+      }
+    ]
   },
   {
-    text: 'Inside which HTML element do we put the JavaScript?',
-    answers: [{ text: "< javascript >", value: 1 }, { text: "< script >", value: 0 }, { text: "< js >", value: 0 }]
+    questions: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+      { text: "< javascript >", value: 0 },
+      { text: "< script >", value: 1 },
+      { text: "< js >", value: 0 }
+    ]
   },
   {
-    text: 'What does HTML stand for?',
-    answers: [{ text: "Home Tool Markup Language", value: 0 }, { text: 'Hyper Text Markup Language', value: 1 }, { text: 'Hyperlinks and Text Markup Language', value: 0 }]
+    questions: "What does HTML stand for?",
+    answers: [
+      { text: "Home Tool Markup Language", value: 0 },
+      { text: "Hyper Text Markup Language", value: 1 },
+      { text: "Hyperlinks and Text Markup Language", value: 0 }
+    ]
   },
   {
-    text: 'Who is making the Web standards?',
-    answers: [{ text: 'Google', value: 0 }, { text: 'The World Wide Web Consortium', value: 1 }, { text: 'Microsoft', value: 0 }]
+    questions: "Who is making the Web standards?",
+    answers: [
+      { text: "Google", value: 0 },
+      { text: "The World Wide Web Consortium", value: 1 },
+      { text: "Microsoft", value: 0 }
+    ]
   },
   {
-    text: 'Choose the correct HTML element for the largest heading.',
-    answers: [{ text: '< head >', value: 0 }, { text: '< h1 >', value: 1 }, { text: '< h6 >', value: 0 }]
+    questions: "Choose the correct HTML element for the largest heading.",
+    answers: [
+      { text: "< head >", value: 0 },
+      { text: "< h1 >", value: 1 },
+      { text: "< h6 >", value: 0 }
+    ]
   },
   {
-    text: 'What is the correct HTML element for inserting a line break?',
-    answers: [{ text: '< break >', value: 0 }, { text: '< lb >', value: 0 }, { text: '< br >', value: 1 }]
+    questions: "What is the correct HTML element for inserting a line break?",
+    answers: [
+      { text: "< break >", value: 0 },
+      { text: "< lb >", value: 0 },
+      { text: "< br >", value: 1 }
+    ]
   },
   {
-    text: 'Where in an HTML document is the correct place to refer to an external style sheet?',
-    answers: [{ text: 'In the < head > section', value: 1 }, { text: 'In the < body > section', value: 0 }, { text: 'At the end of the document', value: 1 }]
+    questions:
+      "Where in an HTML document is the correct place to refer to an external style sheet?",
+    answers: [
+      { text: "In the < head > section", value: 1 },
+      { text: "In the < body > section", value: 0 },
+      { text: "At the end of the document", value: 0 }
+    ]
   },
   {
-    text: 'What is the correct syntax for referring to an external script called "abc.js"?',
-    answers: [{ text: '< script name="abc.js">', value: 0 }, { text: '< script href="abc.js" >', value: 0 }, { text: '< script src="abc.js" >', value: 1 }]
+    questions:
+      'What is the correct syntax for referring to an external script called "abc.js"?',
+    answers: [
+      { text: '< script name="abc.js">', value: 0 },
+      { text: '< script href="abc.js" >', value: 0 },
+      { text: '< script src="abc.js" >', value: 1 }
+    ]
   },
   {
-    text: 'How do you create a function in JavaScript?',
-    answers: [{ text: 'function = myFunction()', value: 0 }, { text: 'function myFunction()', value: 1 }, { text: 'function:myFunction()', value: 0 }]
+    questions: "How do you create a function in JavaScript?",
+    answers: [
+      { text: "function = myFunction()", value: 0 },
+      { text: "function myFunction()", value: 1 },
+      { text: "function:myFunction()", value: 0 }
+    ]
   },
   {
-    text: 'How to write an IF statement in JavaScript?',
-    answers: [{ text: 'if i = 5 then', value: 0 }, { text: 'if i == 5 then', value: 0 }, { text: 'if (i == 5)', value: 1 }]
+    questions: "How to write an IF statement in JavaScript?",
+    answers: [
+      { text: "if i = 5 then", value: 0 },
+      { text: "if i == 5 then", value: 0 },
+      { text: "if (i == 5)", value: 1 }
+    ]
   }
-]
+];
 
+var corectAnswers = 0;
+var compareList = [];
+for (var i = 0; i < list.length; i++) {
+  for (var j = 0; j < list[i].answers.length; j++) {
+    corectAnswers += list[i].answers[j].value;
+  }
+  compareList.push(corectAnswers);
+  corectAnswers = 0;
+}
 
 var userAnswers = [];
 
-// q1
-[[0, 2]]
-
-//q2
-[[0, 2], [2]]
-
-// [...]
-// 0,2
-
-// 2
-
-// sum += questions[i].answers[j].value;
-
-
-
-beginBtn.addEventListener('click', checkUser);
+beginBtn.addEventListener("click", checkUser);
 
 function checkUser() {
-  var myRequest = new XMLHttpRequest;
-  myRequest.open("GET", "https://api.github.com/users/" + gitUser.value + "/repos");
+  var myRequest = new XMLHttpRequest();
+  myRequest.open(
+    "GET",
+    "https://api.github.com/users/" + gitUser.value + "/repos"
+  );
   myRequest.send();
 
   myRequest.addEventListener("load", function onLoad(e) {
@@ -90,16 +135,16 @@ function checkUser() {
     var myResponseAsAJSON = JSON.parse(myResponseAsText);
     var x = myResponseAsAJSON.length;
     //log(a)
-    if (a >= 200 & a < 400) {
+    if ((a >= 200) & (a < 400)) {
       firstPage.classList.add("hidden");
       congratsPage.classList.remove("hidden");
-      repoCount.innerHTML = "Congrats! You have " + x + " repos on Github."
+      repoCount.innerHTML = "Congrats! You have " + x + " repos on Github.";
+    } else {
+      if (a >= 400) {
+        userError.classList.add("error");
+      }
     }
-    else {
-      if (a >= 400) { userError.classList.add("error") }
-    }
-
-  })
+  });
 }
 
 var i = 0;
@@ -110,44 +155,60 @@ letsGo.addEventListener("click", switchToQuiz);
 function switchToQuiz() {
   congratsPage.classList.add("hidden");
   secondPage.classList.remove("hidden");
-  generateQ(0);
+  generateQ(i);
 }
 
-
-NextQuestion.addEventListener('click', generateQuiz);
-
-
+NextQuestion.addEventListener("click", generateQuiz);
+var score = 0;
 function generateQuiz() {
-
-  if (i < questions.length) {
-    storeAnswers(i);
+  storeAnswers(i);
+  if (userAnswers[i].length == 0) {
+    generateQ(i);
+  } else i++;
+  if (i < list.length) {
     myQuestion.innerHTML = "";
     inputForm.innerHTML = "";
     generateQ(i);
 
-    i++;
-  }
-
-  else {
+  } else {
     secondPage.classList.add("hidden");
     lastPage.classList.remove("hidden");
+    for (var a = 0; a < userAnswers.length; a++)
+      for (var b = 0; b < userAnswers[a].length; b++) {
+        if (list[a].answers[userAnswers[b]].value == 0) break;
+        else if (
+          (list[a].answers[userAnswers[a][b]].value == 1 &&
+            b < userAnswers[a].length - 1 &&
+            list[a].answers[userAnswers[a][b + 1]].value == 0) ||
+          (list[a].answers[userAnswers[a][b]].value == 1 &&
+            b < userAnswers[a].length - 2 &&
+            list[a].answers[userAnswers[a][b + 2]].value == 0)
+        ) {
+          break;
+        } else {
+          if (userAnswers[a].length == compareList[a]) score += 1;
+          break;
+        }
+      }
+    log(score);
   }
 }
 
 function generateQ(i) {
-  myQuestion.innerHTML = questions[i].text;
+  myQuestion.innerHTML = list[i].questions;
 
-  for (var j = 0; j < questions[i].answers.length; j++)
-    inputForm.innerHTML += '<input class="checkBox" type = "checkbox" value=' + questions[i].answers[j].value + '>' + questions[i].answers[j].text;
+  for (var j = 0; j < list[i].answers.length; j++)
+    inputForm.innerHTML +=
+      '<input class="checkBox" type = "checkbox" >' + list[i].answers[j].text;
 }
 
 function storeAnswers(i) {
-
-  var inputList = document.getElementsByClassName('checkBox');
+  var inputList = document.getElementsByClassName("checkBox");
   userAnswers.push([]);
   for (var a = 0; a < inputList.length; a++)
-    if (inputList[a].checked == true)
-      userAnswers[i].push(a);
-  log(userAnswers)
+    if (inputList[a].checked == true) userAnswers[i].push(a)
+    else{
+      break
+    }
+  log(userAnswers);
 }
-
